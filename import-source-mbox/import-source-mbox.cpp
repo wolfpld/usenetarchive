f19eec9fe4ea843b216e72bbb9b23eaa2f4e7cd1
Bartosz Taudul <wolf.pld@gmail.com>
2016-06-26 00:43:09 +0200

    Parse mbox files.

--- import-source-mbox/import-source-mbox.cpp
+++ import-source-mbox/import-source-mbox.cpp
@@ -65,70 +65,6 @@ static bool CreateDirStruct( const std::string& path )
     return true;
 }
 
-static std::vector<std::string> ListDirectory( const std::string& path )
-{
-    std::vector<std::string> ret;
-
-#ifdef _WIN32
-    WIN32_FIND_DATA ffd;
-    HANDLE h;
-
-    std::string p = path + "/*";
-    for( unsigned int i=0; i<p.size(); i++ )
-    {
-        if( p[i] == '/' )
-        {
-            p[i] = '\\';
-        }
-    }
-
-    h = FindFirstFile( ( p ).c_str(), &ffd );
-    if( h == INVALID_HANDLE_VALUE )
-    {
-        return ret;
-    }
-
-    do
-    {
-        std::string s = ffd.cFileName;
-        if( s != "." && s != ".." )
-        {
-            if( ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY )
-            {
-                s += "/";
-            }
-            ret.emplace_back( std::move( s ) );
-        }
-    }
-    while( FindNextFile( h, &ffd ) );
-
-    FindClose( h );
-#else
-    DIR* dir = opendir( path.c_str() );
-    if( dir == nullptr )
-    {
-        return ret;
-    }
-
-    struct dirent* ent;
-    while( ( ent = readdir( dir ) ) != nullptr )
-    {
-        std::string s = ent->d_name;
-        if( s != "." && s != ".." )
-        {
-            if( ent->d_type == DT_DIR )
-            {
-                s += "/";
-            }
-            ret.emplace_back( std::move( s ) );
-        }
-    }
-    closedir( dir );
-#endif
-
-    return ret;
-}
-
 class ExpandingBuffer
 {
 public:
@@ -150,6 +86,25 @@ private:
     int m_size = 0;
 };
 
+static bool ReadLine( FILE* f, std::string& out )
+{
+    out.clear();
+
+    for(;;)
+    {
+        char c;
+        const size_t read = fread( &c, 1, 1, f );
+        if( read == 0 || c == '\n' )
+        {
+            return read > 0 || !out.empty();
+        }
+        if( c != '\r' )
+        {
+            out += c;
+        }
+    }
+}
+
 int main( int argc, char** argv )
 {
     if( argc != 3 )
@@ -159,7 +114,7 @@ int main( int argc, char** argv )
     }
     if( !Exists( argv[1] ) )
     {
-        fprintf( stderr, "Source directory doesn't exist.\n" );
+        fprintf( stderr, "Source file doesn't exist.\n" );
         exit( 1 );
     }
     if( Exists( argv[2] ) )
@@ -169,7 +124,7 @@ int main( int argc, char** argv )
     }
 
     CreateDirStruct( argv[2] );
-    const auto list = ListDirectory( argv[1] );
+    FILE* in = fopen( argv[1], "rb" );
 
     std::string metafn = argv[2];
     metafn.append( "/" );
@@ -182,40 +137,42 @@ int main( int argc, char** argv )
     uint64_t offset = 0;
 
     ExpandingBuffer eb1, eb2;
-    char in[1024];
-    int fpos = strlen( argv[1] );
-    memcpy( in, argv[1], fpos );
-    in[fpos++] = '/';
+    std::string line;
+    std::string post;
     int idx = 0;
-    for( const auto& f : list )
+    while( ReadLine( in, line ) )
     {
-        if( ( idx & 0x3FF ) == 0 )
+        if( line.size() > 5 && strncmp( line.data(), "From ", 5 ) == 0 )
         {
-            printf( "%i/%i\r", idx, list.size() );
-            fflush( stdout );
+            if( !post.empty() )
+            {
+                int maxSize = LZ4_compressBound( post.size() );
+                char* compressed = eb2.Request( maxSize );
+                int csize = LZ4_compress_HC( post.data(), compressed, post.size(), maxSize, 16 );
+
+                fwrite( compressed, 1, csize, data );
+
+                RawImportMeta metaPacket = { offset, post.size(), csize };
+                fwrite( &metaPacket, 1, sizeof( RawImportMeta ), meta );
+                offset += csize;
+
+                post.clear();
+
+                if( ( idx & 0x3FF ) == 0 )
+                {
+                    printf( "%i\r", idx );
+                    fflush( stdout );
+                }
+                idx++;
+            }
         }
-        idx++;
-
-        if( f[0] == '.' ) continue;
-
-        strcpy( in+fpos, f.c_str() );
-        uint64_t size = GetFileSize( in );
-        char* buf = eb1.Request( size );
-        FILE* src = fopen( in, "rb" );
-        fread( buf, 1, size, src );
-        fclose( src );
-
-        int maxSize = LZ4_compressBound( size );
-        char* compressed = eb2.Request( maxSize );
-        int csize = LZ4_compress_HC( buf, compressed, size, maxSize, 16 );
-
-        fwrite( compressed, 1, csize, data );
-
-        RawImportMeta metaPacket = { offset, size, csize };
-        fwrite( &metaPacket, 1, sizeof( RawImportMeta ), meta );
-        offset += csize;
+        else
+        {
+            post.append( line );
+            post.append( "\n" );
+        }
     }
-    printf( "%i files processed.\n", list.size() );
+    printf( "%i files processed.\n", idx );
 
     fclose( meta );
     fclose( data );
