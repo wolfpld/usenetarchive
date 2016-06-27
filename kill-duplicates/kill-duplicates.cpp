5a4a221a59ebf0318b68566ab633944a8be781ed
wolf <wolf@DESKTOP-7S27OVU>
2016-06-27 02:01:41 +0200

    Add kill-duplicates tool.

---
+++ kill-duplicates/kill-duplicates.cpp
@@ -0,0 +1,115 @@
+#include <algorithm>
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unordered_set>
+#include <vector>
+
+#include "../contrib/lz4/lz4.h"
+#include "../common/ExpandingBuffer.hpp"
+#include "../common/Filesystem.hpp"
+#include "../common/FileMap.hpp"
+#include "../common/RawImportMeta.hpp"
+#include "../common/String.hpp"
+
+int main( int argc, char** argv )
+{
+    if( argc != 3 )
+    {
+        fprintf( stderr, "USAGE: %s source destination\n", argv[0] );
+        exit( 1 );
+    }
+    if( !Exists( argv[1] ) )
+    {
+        fprintf( stderr, "Source directory doesn't exist.\n" );
+        exit( 1 );
+    }
+    if( Exists( argv[2] ) )
+    {
+        fprintf( stderr, "Destination directory already exists.\n" );
+        exit( 1 );
+    }
+
+    CreateDirStruct( argv[2] );
+
+    std::string base = argv[1];
+    base.append( "/" );
+    std::string metafn = base + "meta";
+    std::string datafn = base + "data";
+
+    if( !Exists( metafn ) || !Exists( datafn ) )
+    {
+        fprintf( stderr, "Raw data files do not exist.\n" );
+        exit( 1 );
+    }
+
+    FileMap<RawImportMeta> meta( metafn );
+    FileMap<char> data( datafn );
+
+    auto size = meta.Size() / sizeof( RawImportMeta );
+
+    std::string dbase = argv[2];
+    dbase.append( "/" );
+    std::string dmetafn = dbase + "meta";
+    std::string ddatafn = dbase + "data";
+
+    FILE* dmeta = fopen( dmetafn.c_str(), "wb" );
+    FILE* ddata = fopen( ddatafn.c_str(), "wb" );
+
+    ExpandingBuffer eb;
+    uint32_t cntd = 0;
+    uint32_t cntu = 0;
+    std::unordered_set<std::string> unique;
+    uint64_t offset = 0;
+    for( uint32_t i=0; i<size; i++ )
+    {
+        if( ( i & 0x3FF ) == 0 )
+        {
+            printf( "%i/%i\r", i, size );
+            fflush( stdout );
+        }
+
+        auto postsize = meta[i].size;
+        auto post = eb.Request( postsize );
+        auto dec = LZ4_decompress_fast( data + meta[i].offset, post, postsize );
+        auto buf = post;
+        assert( dec == meta[i].compressedSize );
+
+        while( strnicmpl( buf, "message-id: <", 13 ) != 0 )
+        {
+            buf++;
+            while( *buf++ != '\n' ) {}
+        }
+        buf += 13;
+        auto end = buf;
+        while( *end != '>' ) end++;
+
+        std::string tmp( buf, end );
+        if( unique.find( tmp ) == unique.end() )
+        {
+            unique.emplace( std::move( tmp ) );
+            cntu++;
+
+            fwrite( data + meta[i].offset, 1, meta[i].compressedSize, ddata );
+
+            RawImportMeta metaPacket = { offset, meta[i].size, meta[i].compressedSize };
+            fwrite( &metaPacket, 1, sizeof( RawImportMeta ), dmeta );
+            offset += meta[i].compressedSize;
+        }
+        else
+        {
+            cntd++;
+        }
+    }
+
+    fclose( dmeta );
+    fclose( ddata );
+
+    printf( "Processed %i MsgIDs. Unique: %i, dupes: %i\n", size, cntu, cntd );
+
+    return 0;
+}
