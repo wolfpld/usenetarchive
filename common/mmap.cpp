97da8b2225e4792dbc9a818f3eaf9e0bd315ad4b
Bartosz Taudul <wolf.pld@gmail.com>
2016-06-29 01:45:10 +0200

    Properly map files greater than 4GB.

--- common/mmap.cpp
+++ common/mmap.cpp
@@ -12,14 +12,14 @@ void* mmap( void* addr, size_t length, int prot, int flags, int fd, off_t offset
     switch( prot )
     {
     case PROT_READ:
-        if( hnd = CreateFileMapping( HANDLE( _get_osfhandle( fd ) ), nullptr, PAGE_READONLY, 0, DWORD( length ), nullptr ) )
+        if( hnd = CreateFileMapping( HANDLE( _get_osfhandle( fd ) ), nullptr, PAGE_READONLY, 0, 0, nullptr ) )
         {
             map = MapViewOfFile( hnd, FILE_MAP_READ, 0, 0, length );
             CloseHandle( hnd );
         }
         break;
     case PROT_WRITE:
-        if( hnd = CreateFileMapping( HANDLE( _get_osfhandle( fd ) ), nullptr, PAGE_READWRITE, 0, DWORD( length ), nullptr ) )
+        if( hnd = CreateFileMapping( HANDLE( _get_osfhandle( fd ) ), nullptr, PAGE_READWRITE, 0, 0, nullptr ) )
         {
             map = MapViewOfFile( hnd, FILE_MAP_WRITE, 0, 0, length );
             CloseHandle( hnd );
