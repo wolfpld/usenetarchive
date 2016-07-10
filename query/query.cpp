8927179a4590a4524bbcad54e54c2b959b62ef81
Bartosz Taudul <wolf.pld@gmail.com>
2016-07-10 21:49:47 +0200

    User interface for libuat.

--- query/query.cpp
+++ query/query.cpp
@@ -1,122 +1,52 @@
-#include <assert.h>
-#include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string>
-#include <string.h>
+#include <stdio.h>
+
+#include "../contrib/linenoise-ng/linenoise.h"
 
 #include "../common/Filesystem.hpp"
-#include "../common/HashSearch.hpp"
-#include "../common/MessageView.hpp"
-#include "../common/MetaView.hpp"
-#include "../common/MsgIdHash.hpp"
-#include "../common/String.hpp"
+#include "../libuat/Archive.hpp"
 
-#define CSTR(x) strcmp( argv[2], x ) == 0
-
-enum class Mode
+void PrintHelp()
 {
-    Invalid,
-    Info,
-    List,
-    QueryMsgId,
-    QueryPost
-};
+    printf( "view idx - view message of given idx\n" );
+}
 
 int main( int argc, char** argv )
 {
-    if( argc < 3 )
+    if( argc < 2 )
     {
-        fprintf( stderr, "USAGE: %s data params\n\nParams:\n", argv[0] );
-        fprintf( stderr, "  -i            general information\n" );
-        fprintf( stderr, "  -l            list all Message IDs\n" );
-        fprintf( stderr, "  -m msgid      query Message ID\n" );
-        fprintf( stderr, "  -q id         query post\n" );
+        fprintf( stderr, "USAGE: %s archive\n", argv[0] );
         exit( 1 );
     }
     if( !Exists( argv[1] ) )
     {
-        fprintf( stderr, "Source directory doesn't exist.\n" );
+        fprintf( stderr, "Archive doesn't exist.\n" );
         exit( 1 );
     }
 
-    Mode mode = Mode::Invalid;
+    Archive archive( argv[1] );
 
-    if( CSTR( "-i" ) )
+    printf( "Usenet archive %s opened. Number of messages: %i\n", argv[1], archive.NumberOfMessages() );
+
+    while( char* cmd = linenoise( "\x1b[1;32mcmd>\x1b[0m " ) )
     {
-        mode = Mode::Info;
-    }
-    else if( CSTR( "-l" ) )
-    {
-        mode = Mode::List;
-    }
-    else if( CSTR( "-m" ) )
-    {
-        if( argc < 4 )
+        if( strncmp( cmd, "view ", 5 ) == 0 )
         {
-            fprintf( stderr, "Missing Message ID.\n" );
-            exit( 1 );
+            int idx = atoi( cmd+5 );
+            auto msg = archive.GetMessage( idx );
+            if( msg )
+            {
+                printf( "%s\n", msg );
+            }
+            else
+            {
+                printf( "Invalid message index (max %i).\n", archive.NumberOfMessages() );
+            }
         }
-        mode = Mode::QueryMsgId;
-    }
-    else if( CSTR( "-q" ) )
-    {
-        if( argc < 4 )
+        else
         {
-            fprintf( stderr, "Missing post number.\n" );
-            exit( 1 );
+            PrintHelp();
         }
-        mode = Mode::QueryPost;
-    }
-
-    if( mode == Mode::Invalid )
-    {
-        fprintf( stderr, "Invalid mode of operation.\n" );
-        exit( 1 );
-    }
-
-    std::string base = argv[1];
-    base.append( "/" );
-
-    MessageView mview( base + "meta", base + "data" );
-    const MetaView<uint32_t, char> mid( base + "midmeta", base + "middata" );
-    const HashSearch hash( base + "middata", base + "midhash", base + "midhashdata" );
-
-    const auto size = mview.Size();
-
-    if( mode == Mode::Info )
-    {
-        printf( "Raw archive %s\n", argv[1] );
-        printf( "  Number of posts: %i\n", size );
-    }
-    else if( mode == Mode::List )
-    {
-        for( uint32_t i=0; i<size; i++ )
-        {
-            const char* payload = mid[i];
-            printf( "%s\n", payload );
-        }
-    }
-    else if( mode == Mode::QueryMsgId )
-    {
-        auto idx = hash.Search( argv[3] );
-        if( idx >= 0 )
-        {
-            printf( "%s\n", mview[idx] );
-            return 0;
-        }
-        fprintf( stderr, "Invalid Message ID %s\n", argv[3] );
-        exit( 1 );
-    }
-    else if( mode == Mode::QueryPost )
-    {
-        auto idx = atoi( argv[3] );
-        if( idx < 0 || idx >= size )
-        {
-            fprintf( stderr, "Index out of range: 0 <= %i < %i\n", idx, size );
-            exit( 1 );
-        }
-        printf( "%s\n", mview[idx] );
     }
 
     return 0;
