212a4b3b2ac3218e46b50e62a7daf00ac272d32c
Bartosz Taudul <wolf.pld@gmail.com>
2016-07-11 22:01:31 +0200

    Setup dark theme.
    Slightly modified https://gist.github.com/QuantumCD/6245215

--- browser/main.cpp
+++ browser/main.cpp
@@ -1,10 +1,33 @@
 #include <QApplication>
+#include <QStyleFactory>
 
 #include "browser.h"
 
 int main( int argc, char *argv[] )
 {
     QApplication a( argc, argv );
+
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+    darkPalette.setColor(QPalette::WindowText, QColor(212,212,212));
+    darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, QColor(212,212,212));
+    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ButtonText, QColor(212,212,212));
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    qApp->setPalette(darkPalette);
+    qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+
     Browser w;
     w.show();
 
