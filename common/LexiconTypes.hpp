219187538e69b1ab22d123f759a5726295f2d8ab
Bartosz Taudul <wolf.pld@gmail.com>
2016-07-18 20:24:05 +0200

    Show bit layout in comments.

--- common/LexiconTypes.hpp
+++ common/LexiconTypes.hpp
@@ -25,21 +25,21 @@ enum { LexiconChildMask = 0xF8000000 };
 enum { LexiconChildShift = 27 };
 
 const uint8_t LexiconHitTypeEncoding[] = {
-    0x00,
-    0xE0,
-    0x80,
-    0xA0,
-    0xC0,
-    0xF0
+    0x00,   // 0000 0000
+    0xE0,   // 1110 0000
+    0x80,   // 1000 0000
+    0xA0,   // 1010 0000
+    0xC0,   // 1100 0000
+    0xF0    // 1111 0000
 };
 
 const uint8_t LexiconHitPosMask[] = {
-    0x7F,
-    0x1F,
-    0x1F,
-    0x1F,
-    0x0F,
-    0x0F
+    0x7F,   // 0111 1111
+    0x1F,   // 0001 1111
+    0x1F,   // 0001 1111
+    0x1F,   // 0001 1111
+    0x0F,   // 0000 1111
+    0x0F    // 0000 1111
 };
 
 static inline LexiconType LexiconDecodeType( uint8_t v )
