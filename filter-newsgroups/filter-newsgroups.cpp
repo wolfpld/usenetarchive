73656ddf74eed837ac02ca0a7c8916d29ec88cd2
Bartosz Taudul <wolf.pld@gmail.com>
2016-07-23 15:55:30 +0200

    Filter out messages that were not sent to the newsgroup.

--- filter-newsgroups/filter-newsgroups.cpp
+++ filter-newsgroups/filter-newsgroups.cpp
@@ -6,7 +6,6 @@
 #include <string>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unordered_set>
 #include <vector>
 
 #include "../common/ExpandingBuffer.hpp"
@@ -18,31 +17,31 @@
 
 int main( int argc, char** argv )
 {
-    if( argc != 3 )
+    if( argc != 4 )
     {
-        fprintf( stderr, "USAGE: %s source destination\n", argv[0] );
+        fprintf( stderr, "USAGE: %s newsgroup source destination\n", argv[0] );
         exit( 1 );
     }
-    if( !Exists( argv[1] ) )
+    if( !Exists( argv[2] ) )
     {
         fprintf( stderr, "Source directory doesn't exist.\n" );
         exit( 1 );
     }
-    if( Exists( argv[2] ) )
+    if( Exists( argv[3] ) )
     {
         fprintf( stderr, "Destination directory already exists.\n" );
         exit( 1 );
     }
 
-    CreateDirStruct( argv[2] );
+    CreateDirStruct( argv[3] );
 
-    std::string base = argv[1];
+    std::string base = argv[2];
     base.append( "/" );
 
     MessageView mview( base + "meta", base + "data" );
     const auto size = mview.Size();
 
-    std::string dbase = argv[2];
+    std::string dbase = argv[3];
     dbase.append( "/" );
     std::string dmetafn = dbase + "meta";
     std::string ddatafn = dbase + "data";
@@ -50,10 +49,11 @@ int main( int argc, char** argv )
     FILE* dmeta = fopen( dmetafn.c_str(), "wb" );
     FILE* ddata = fopen( ddatafn.c_str(), "wb" );
 
+    const auto matchlen = strlen( argv[1] );
+
     ExpandingBuffer eb;
-    uint32_t cntd = 0;
-    uint32_t cntu = 0;
-    std::unordered_set<std::string> unique;
+    uint32_t cntgood = 0;
+    uint64_t savec = 0, saveu = 0;
     uint64_t offset = 0;
     for( uint32_t i=0; i<size; i++ )
     {
@@ -66,38 +66,49 @@ int main( int argc, char** argv )
         auto post = mview[i];
         auto buf = post;
 
-        while( strnicmpl( buf, "message-id: <", 13 ) != 0 )
+        while( strnicmpl( buf, "newsgroups: ", 12 ) != 0 )
         {
             buf++;
             while( *buf++ != '\n' ) {}
         }
-        buf += 13;
+        buf += 12;
         auto end = buf;
-        while( *end != '>' ) end++;
-
-        std::string tmp( buf, end );
-        if( unique.find( tmp ) == unique.end() )
+        bool good = false;
+        while( *end != '\n' )
         {
-            unique.emplace( std::move( tmp ) );
-            cntu++;
+            while( *end == ' ' || *end == ',' ) end++;
+            buf = end;
+            while( *end != '\n' && *end != ',' ) end++;
+            if( end-buf == matchlen )
+            {
+                if( strncmp( buf, argv[1], matchlen ) == 0 )
+                {
+                    const auto raw = mview.Raw( i );
+                    fwrite( raw.ptr, 1, raw.compressedSize, ddata );
 
-            const auto raw = mview.Raw( i );
-            fwrite( raw.ptr, 1, raw.compressedSize, ddata );
+                    RawImportMeta metaPacket = { offset, raw.size, raw.compressedSize };
+                    fwrite( &metaPacket, 1, sizeof( RawImportMeta ), dmeta );
+                    offset += raw.compressedSize;
 
-            RawImportMeta metaPacket = { offset, raw.size, raw.compressedSize };
-            fwrite( &metaPacket, 1, sizeof( RawImportMeta ), dmeta );
-            offset += raw.compressedSize;
+                    cntgood++;
+                    good = true;
+                    break;
+                }
+            }
         }
-        else
+        if( !good )
         {
-            cntd++;
+            const auto raw = mview.Raw( i );
+            savec += raw.compressedSize;
+            saveu += raw.size;
         }
     }
 
     fclose( dmeta );
     fclose( ddata );
 
-    printf( "Processed %i MsgIDs. Unique: %i, dupes: %i\n", size, cntu, cntd );
+    printf( "Processed %i messages. Valid newsgroup: %i, bogus messages: %i\n", size, cntgood, size - cntgood );
+    printf( "Saved %i KB (uncompressed), %i KB (compressed)\n", saveu / 1024, savec / 1024 );
 
     return 0;
 }
