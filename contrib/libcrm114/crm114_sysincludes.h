4df3f4915c6e99445962462e6db3af804c4845dd
Bartosz Taudul <wolf.pld@gmail.com>
2016-07-27 01:19:48 +0200

    For fucks sake.

--- contrib/libcrm114/crm114_sysincludes.h
+++ contrib/libcrm114/crm114_sysincludes.h
@@ -40,53 +40,11 @@
 #include <string.h>
 #include <sys/types.h>
 
-// Detect if compilation is occurring in a Microsoft compiler
-#if (defined (WIN32) || defined (WIN64) || defined (_WIN32) || defined (_WIN64))
-#define CRM_WINDOWS
-#endif
-
-#ifndef CRM_WINDOWS
-
-#include <inttypes.h>
-
-#else
-
-//   Windows specific declarations follow
-
-// Apparently Microsoft's C compiler (Visual Studio C++) is C89, with
-// a few extensions.  No C99 "extended integer types" -- no stdint.h
-// or inttypes.h -- but has a non-standard thing built into the
-// compiler.
-
-typedef  __int64 int64_t;
-typedef  __int32 int32_t;
-typedef  __int16 int16_t;
-typedef  __int8  int8_t;
-
-typedef  unsigned __int64 uint64_t;
-typedef  unsigned __int32 uint32_t;
-typedef  unsigned __int16 uint16_t;
-typedef  unsigned __int8  uint8_t;
-
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-#include <direct.h>
-#include <io.h>
-#include <windows.h>
+#include <stdint.h>
 
 #define snprintf _snprintf
 #define strncasecmp(a,b,n) strnicmp((a), (b), (n))
 #define strcasecmp(a,b) stricmp((a), (b))
 typedef long int clock_t;
-
-// Microsoft's C compiler is C89, which doesn't include inline
-// functions, but Microsoft Web documentation of the C compiled by
-// Visual Studio C++ says it has inline functions as an extension to
-// C89, but not using the usual keyword: uses keyword "__inline".
-// So here is a hack to use that keyword for Microsoft.
-
-#define inline __inline
-
-#endif	// CRM_WINDOWS
 
 #endif	// !__CRM114_SYSINCLUDES_H__
