1b6af2810f5d7c752f7bc520411acc099616217b
Bartosz Taudul <wolf.pld@gmail.com>
2016-07-27 02:36:50 +0200

    Initial try at spam training.

--- filter-spam/filter-spam.cpp
+++ filter-spam/filter-spam.cpp
@@ -12,105 +12,155 @@
 #include "../common/Filesystem.hpp"
 #include "../common/FileMap.hpp"
 #include "../common/MessageView.hpp"
+#include "../common/MetaView.hpp"
 #include "../common/RawImportMeta.hpp"
 #include "../common/String.hpp"
 
+extern "C" {
+#include "../contrib/libcrm114/crm114_sysincludes.h"
+#include "../contrib/libcrm114/crm114_config.h"
+#include "../contrib/libcrm114/crm114_structs.h"
+#include "../contrib/libcrm114/crm114_lib.h"
+#include "../contrib/libcrm114/crm114_internal.h"
+}
+
+#ifdef _WIN32
+#  include <windows.h>
+#endif
+
 int main( int argc, char** argv )
 {
-    if( argc != 4 )
+#ifdef _WIN32
+    SetConsoleMode( GetStdHandle( STD_OUTPUT_HANDLE ), 0x07 );
+#endif
+
+    if( argc != 3 && argc != 4 )
     {
-        fprintf( stderr, "USAGE: %s newsgroup source destination\n", argv[0] );
+        fprintf( stderr, "USAGE: %s database source [destination]\n", argv[0] );
+        fprintf( stderr, "Omitting destination will start training mode.\n" );
         exit( 1 );
     }
+
+    bool training = argc == 3;
+
     if( !Exists( argv[2] ) )
     {
         fprintf( stderr, "Source directory doesn't exist.\n" );
         exit( 1 );
     }
-    if( Exists( argv[3] ) )
-    {
-        fprintf( stderr, "Destination directory already exists.\n" );
-        exit( 1 );
-    }
-
-    CreateDirStruct( argv[3] );
 
     std::string base = argv[2];
     base.append( "/" );
 
     MessageView mview( base + "meta", base + "data" );
     const auto size = mview.Size();
+    FileMap<uint32_t> toplevel( base + "toplevel" );
+    auto topsize = toplevel.DataSize();
+    MetaView<uint32_t, char> strings( base + "strmeta", base + "strings" );
+    MetaView<uint32_t, uint32_t> conn( base + "connmeta", base + "conndata" );
 
-    std::string dbase = argv[3];
-    dbase.append( "/" );
-    std::string dmetafn = dbase + "meta";
-    std::string ddatafn = dbase + "data";
+    CRM114_CONTROLBLOCK* crm_cb = crm114_new_cb();
+    crm114_cb_setflags( crm_cb, CRM114_FSCM );
+    crm114_cb_setclassdefaults( crm_cb );
+    crm_cb->how_many_classes = 2;
+    strcpy( crm_cb->cls[0].name, "valid" );
+    strcpy( crm_cb->cls[1].name, "spam" );
+    crm_cb->datablock_size = 1 << 25;
+    crm114_cb_setblockdefaults( crm_cb );
 
-    FILE* dmeta = fopen( dmetafn.c_str(), "wb" );
-    FILE* ddata = fopen( ddatafn.c_str(), "wb" );
+    CRM114_DATABLOCK* crm_db = crm114_new_db( crm_cb );
 
-    const auto matchlen = strlen( argv[1] );
-
-    ExpandingBuffer eb;
-    uint32_t cntgood = 0;
-    uint64_t savec = 0, saveu = 0;
-    uint64_t offset = 0;
-    for( uint32_t i=0; i<size; i++ )
+    if( !training )
     {
-        if( ( i & 0x3FF ) == 0 )
+        if( !Exists( argv[1] ) )
         {
-            printf( "%i/%i\r", i, size );
-            fflush( stdout );
+            fprintf( stderr, "Spam database not present.\n" );
+            exit( 1 );
+        }
+        if( Exists( argv[3] ) )
+        {
+            fprintf( stderr, "Destination directory already exists.\n" );
+            exit( 1 );
         }
 
-        auto post = mview[i];
-        auto buf = post;
-        auto raw = mview.Raw( i );
-
-        while( strnicmpl( buf, "newsgroups: ", 12 ) != 0 && buf - post < raw.size )
+        CreateDirStruct( argv[3] );
+    }
+    else
+    {
+        if( !Exists( argv[1] ) )
         {
-            buf++;
-            while( *buf++ != '\n' && buf - post < raw.size ) {}
+            CreateDirStruct( argv[1] );
         }
-        if( buf - post >= raw.size ) continue;
-        buf += 12;
-        auto end = buf;
-        bool good = false;
-        while( *end != '\n' )
+
+        printf( "Spam training mode.\n" );
+
+        for( uint32_t i=0; i<topsize; i++ )
         {
-            while( *end == ' ' || *end == ',' ) end++;
-            buf = end;
-            while( *end != '\n' && *end != ',' ) end++;
-            if( end-buf == matchlen )
+            auto idx = toplevel[i];
+            auto children = conn[idx];
+            children += 2;
+            if( *children != 0 ) continue;
+
+            auto post = mview[idx];
+            auto raw = mview.Raw( idx );
+
+            printf( "\n\t\033[36;1m-= Message %i =-\033[0m\n\n", idx );
+            printf( "\033[35;1mFrom: %s\n\033[33;1mSubject: %s\033[0m\n\n", strings[idx*3], strings[idx*3+1] );
+
+            auto ptr = post;
+            auto end = ptr;
+            bool done = false;
+            do
             {
-                if( strncmp( buf, argv[1], matchlen ) == 0 )
+                end = ptr;
+                while( *end != '\n' ) end++;
+                if( end - ptr == 0 )
                 {
-                    const auto raw = mview.Raw( i );
-                    fwrite( raw.ptr, 1, raw.compressedSize, ddata );
-
-                    RawImportMeta metaPacket = { offset, raw.size, raw.compressedSize };
-                    fwrite( &metaPacket, 1, sizeof( RawImportMeta ), dmeta );
-                    offset += raw.compressedSize;
-
-                    cntgood++;
-                    good = true;
-                    break;
+                    while( *end == '\n' ) end++;
+                    end--;
+                    done = true;
                 }
+                ptr = end + 1;
             }
-        }
-        if( !good )
-        {
-            const auto raw = mview.Raw( i );
-            savec += raw.compressedSize;
-            saveu += raw.size;
+            while( !done );
+
+            for( int i=0; i<8; i++ )
+            {
+                end = ptr;
+                while( *end != '\n' && *end != '\0' ) end++;
+                while( ptr != end )
+                {
+                    fputc( *ptr++, stdout );
+                }
+                fputc( '\n', stdout );
+                if( *end == '\0' ) break;
+                ptr = end + 1;
+            }
+
+            CRM114_MATCHRESULT res;
+            crm114_classify_text( crm_db, post, raw.size, &res );
+
+            if( res.bestmatch_index == 0 )
+            {
+                printf( "\033[32mClassification: valid" );
+            }
+            else
+            {
+                printf( "\033[31mClassification: spam" );
+            }
+            printf( "   success probability: %.3f.\033[0m\n", res.tsprob );
+            printf( "Press [s] for spam or [v] for valid.\n" );
+
+            char c;
+            do
+            {
+                c = getchar();
+            }
+            while( c != 's' && c != 'v' );
+
+            crm114_learn_text( &crm_db, (c == 's') ? 1 : 0, post, raw.size );
         }
     }
-
-    fclose( dmeta );
-    fclose( ddata );
-
-    printf( "Processed %i messages. Valid newsgroup: %i, bogus messages: %i\n", size, cntgood, size - cntgood );
-    printf( "Saved %i KB (uncompressed), %i KB (compressed)\n", saveu / 1024, savec / 1024 );
 
     return 0;
 }
