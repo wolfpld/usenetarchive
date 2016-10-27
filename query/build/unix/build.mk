c610fa9a192208cae2626ab042ed1be00d31ed4b
Bartosz Taudul <wolf.pld@gmail.com>
2016-10-27 02:35:09 +0200

    Link query with pthreads.

--- query/build/unix/build.mk
+++ query/build/unix/build.mk
@@ -2,7 +2,7 @@ CFLAGS +=
 CXXFLAGS := $(CFLAGS) -std=c++14
 DEFINES +=
 INCLUDES := -I../../../contrib/zstd/common -I../../../contrib/zstd
-LIBS :=
+LIBS := -lpthread
 IMAGE := query
 
 SRC := $(shell egrep 'ClCompile.*cpp"' ../win32/$(IMAGE).vcxproj | sed -e 's/.*\"\(.*\)\".*/\1/' | sed -e 's@\\@/@g')
