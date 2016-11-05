69fb258d3d076fc6d8161649b89920dd931accd6
Bartosz Taudul <wolf.pld@gmail.com>
2016-11-06 00:42:20 +0100

    Verify that archive can be opened.

--- tbrowser/tbrowser.cpp
+++ tbrowser/tbrowser.cpp
@@ -1,5 +1,8 @@
+#include <memory>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "../libuat/Archive.hpp"
 
 int main( int argc, char** argv )
 {
@@ -7,6 +10,12 @@ int main( int argc, char** argv )
     {
         fprintf( stderr, "Usage: %s archive-name.usenet\n", argv[0] );
         return 1;
+    }
+
+    std::unique_ptr<Archive> archive( Archive::Open( argv[1] ) );
+    if( !archive )
+    {
+        fprintf( stderr, "%s is not an archive!\n", argv[1] );
     }
 
     return 0;
