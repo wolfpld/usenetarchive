a9e82f243ff2b08eb47bcf65520ee126923336be
Bartosz Taudul <wolf.pld@gmail.com>
2016-11-06 20:50:42 +0100

    Launch browser.

--- tbrowser/tbrowser.cpp
+++ tbrowser/tbrowser.cpp
@@ -1,10 +1,10 @@
 #include <memory>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <curses.h>
-
 #include "../libuat/Archive.hpp"
+
+#include "Browser.hpp"
 
 int main( int argc, char** argv )
 {
@@ -28,6 +28,16 @@ int main( int argc, char** argv )
         fprintf( stderr, "Terminal doesn't support colors.\n" );
         exit( 1 );
     }
+
+    start_color();
+    cbreak();
+    noecho();
+    keypad( stdscr, TRUE );
+
+    init_pair( 1, COLOR_WHITE, COLOR_BLUE );
+
+    Browser browser( std::move( archive ) );
+    browser.Entry();
 
     endwin();
 
