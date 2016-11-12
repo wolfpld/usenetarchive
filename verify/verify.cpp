82a43e500ce974639ae335b620abc0a7a35e659e
Bartosz Taudul <wolf.pld@gmail.com>
2016-11-12 18:06:52 +0100

    Find unreachable messages.

--- verify/verify.cpp
+++ verify/verify.cpp
@@ -3,8 +3,19 @@
 #include <stdlib.h>
 #include <string>
 #include <string.h>
+#include <unordered_set>
 
 #include "../libuat/Archive.hpp"
+
+void RecursiveRemove( int idx, std::unordered_set<std::string>& data, const Archive& archive )
+{
+    data.erase( data.find( archive.GetMessageId( idx ) ) );
+    const auto children = archive.GetChildren( idx );
+    for( int i=0; i<children.size; i++ )
+    {
+        RecursiveRemove( children.ptr[i], data, archive );
+    }
+}
 
 int main( int argc, char** argv )
 {
@@ -15,6 +26,37 @@ int main( int argc, char** argv )
     }
 
     auto archive = Archive::Open( argv[1] );
+    if( !archive )
+    {
+        fprintf( stderr, "Cannot open archive %s\n", argv[1] );
+        exit( 1 );
+    }
+
+    printf( "Verifying message reachibility...\n" );
+    fflush( stdout );
+    std::unordered_set<std::string> messages;
+    const auto size = archive->NumberOfMessages();
+    for( int i=0; i<size; i++ )
+    {
+        messages.emplace( archive->GetMessageId( i ) );
+    }
+    const auto top = archive->GetTopLevel();
+    for( int i=0; i<top.size; i++ )
+    {
+        RecursiveRemove( top.ptr[i], messages, *archive );
+    }
+    if( messages.empty() )
+    {
+        printf( "All messages reachable\n" );
+    }
+    else
+    {
+        printf( "%i messages unreachable:\n", messages.size() );
+        for( auto& v : messages )
+        {
+            printf( "%s\n", v.c_str() );
+        }
+    }
 
     return 0;
 }
