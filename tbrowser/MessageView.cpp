3c3a947126b167cc382ad8d4864d40074342a863
Bartosz Taudul <wolf.pld@gmail.com>
2016-11-13 17:56:06 +0100

    Add dummy MessageView class.

---
+++ tbrowser/MessageView.cpp
@@ -0,0 +1,10 @@
+#include "MessageView.hpp"
+
+MessageView::MessageView()
+    : View( 0, 0, 1, 1 )
+{
+}
+
+MessageView::~MessageView()
+{
+}
---
+++ tbrowser/MessageView.hpp
@@ -0,0 +1,15 @@
+#ifndef __MESSAGEVIEW_HPP__
+#define __MESSAGEVIEW_HPP__
+
+#include "View.hpp"
+
+class MessageView : public View
+{
+public:
+    MessageView();
+    ~MessageView();
+
+private:
+};
+
+#endif
