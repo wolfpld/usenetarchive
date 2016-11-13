fa8f3f36cb6f94faaf119c053d5709c962356ccb
Bartosz Taudul <wolf.pld@gmail.com>
2016-11-13 03:00:02 +0100

    Safe utf-8 printing.
    Allow printing text without C0 control codes having effect and/or without
    caret notation.

--- tbrowser/UTF8.cpp
+++ tbrowser/UTF8.cpp
@@ -51,3 +51,63 @@ const char* utfendcrlf( const char* str, int len )
     }
     return str;
 }
+
+void utfprint( WINDOW* win, const char* str )
+{
+    auto end = str;
+    while( *end )
+    {
+        if( ( *end & 0x80 ) == 0 )
+        {
+            if( *end <= 0x1F || *end == 0x7F )
+            {
+                wprintw( win, "%.*s", end - str, str );
+                waddch( win, ' ' );
+                end++;
+                str = end;
+            }
+            else
+            {
+                end++;
+            }
+        }
+        else
+        {
+            end += codepointlen( *end );
+        }
+    }
+    if( end != str )
+    {
+        wprintw( win, "%.*s", end - str, str );
+    }
+}
+
+void utfprint( WINDOW* win, const char* str, const char* _end )
+{
+    auto end = str;
+    while( *end && end != _end )
+    {
+        if( ( *end & 0x80 ) == 0 )
+        {
+            if( *end <= 0x1F || *end == 0x7F )
+            {
+                wprintw( win, "%.*s", end - str, str );
+                waddch( win, ' ' );
+                end++;
+                str = end;
+            }
+            else
+            {
+                end++;
+            }
+        }
+        else
+        {
+            end += codepointlen( *end );
+        }
+    }
+    if( end != str )
+    {
+        wprintw( win, "%.*s", end - str, str );
+    }
+}
--- tbrowser/UTF8.hpp
+++ tbrowser/UTF8.hpp
@@ -2,10 +2,14 @@
 #define __UTF8_HPP__
 
 #include <stdlib.h>
+#include <curses.h>
 
 size_t utflen( const char* str );
 const char* utfend( const char* str, int len );
 const char* utfendl( const char* str, int& len );
 const char* utfendcrlf( const char* str, int len );
+
+void utfprint( WINDOW* win, const char* str );
+void utfprint( WINDOW* win, const char* str, const char* end );
 
 #endif
