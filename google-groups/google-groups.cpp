d5dc7d7d2962abfa966d1a3c9f5de03c92e220b2
Bartosz Taudul <wolf.pld@gmail.com>
2016-11-22 00:39:36 +0100

    Init curl.

--- google-groups/google-groups.cpp
+++ google-groups/google-groups.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string>
 #include <string.h>
+#include <curl/curl.h>
 
 #include "../common/Filesystem.hpp"
 
@@ -19,7 +20,15 @@ int main( int argc, char** argv )
         fprintf( stderr, "Source directory doesn't exist.\n" );
         exit( 1 );
     }
-    CreateDirStruct( argv[1] );
+    if( curl_global_init( CURL_GLOBAL_ALL ) != 0 )
+    {
+        fprintf( stderr, "Cannot initialize libcurl.\n" );
+        exit( 1 );
+    }
+
+
+
+    //CreateDirStruct( argv[1] );
 
     std::string base = argv[1];
     base.append( "/" );
