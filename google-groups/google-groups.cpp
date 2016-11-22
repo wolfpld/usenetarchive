bd342235041ffd8ef8e8234b60cd78ccdad83b20
Bartosz Taudul <wolf.pld@gmail.com>
2016-11-22 18:44:25 +0100

    Implement HTTP fetcher.

--- google-groups/google-groups.cpp
+++ google-groups/google-groups.cpp
@@ -10,6 +10,37 @@
 
 #include "../common/Filesystem.hpp"
 
+static size_t WriteFn( void* _data, size_t size, size_t num, void* ptr )
+{
+    const auto data = (unsigned char*)_data;
+    const auto sz = size*num;
+    auto& v = *(std::vector<unsigned char>*)ptr;
+    v.insert( v.end(), data, data+sz );
+    return sz;
+}
+
+static std::vector<unsigned char> Fetch( const std::string& url )
+{
+    std::vector<unsigned char> buf;
+
+    buf.reserve( 64 * 1024 );
+    auto curl = curl_easy_init();
+    if( curl )
+    {
+        curl_easy_setopt( curl, CURLOPT_URL, url.c_str() );
+        curl_easy_setopt( curl, CURLOPT_FOLLOWLOCATION, 1L );
+        curl_easy_setopt( curl, CURLOPT_WRITEFUNCTION, WriteFn );
+        curl_easy_setopt( curl, CURLOPT_WRITEDATA, &buf );
+        auto res = curl_easy_perform( curl );
+        if( res != CURLE_OK )
+        {
+            buf.clear();
+        }
+        curl_easy_cleanup( curl );
+    }
+    return buf;
+}
+
 int main( int argc, char** argv )
 {
     if( argc != 2 )
