7073e29c34880f0d7eacba7718c7dd73d9f37418
Bartosz Taudul <wolf.pld@gmail.com>
2016-11-22 22:28:29 +0100

    OpenSSL threading copypasta.
    https://curl.haxx.se/libcurl/c/opensslthreadlock.html

--- google-groups/google-groups.cpp
+++ google-groups/google-groups.cpp
@@ -12,6 +12,71 @@
 #include "../common/Filesystem.hpp"
 #include "../common/TaskDispatch.hpp"
 
+#ifndef _MSC_VER
+#include <pthread.h>
+#include <openssl/err.h>
+
+#define MUTEX_TYPE       pthread_mutex_t
+#define MUTEX_SETUP(x)   pthread_mutex_init(&(x), NULL)
+#define MUTEX_CLEANUP(x) pthread_mutex_destroy(&(x))
+#define MUTEX_LOCK(x)    pthread_mutex_lock(&(x))
+#define MUTEX_UNLOCK(x)  pthread_mutex_unlock(&(x))
+#define THREAD_ID        pthread_self()
+
+
+void handle_error(const char *file, int lineno, const char *msg)
+{
+    fprintf(stderr, "** %s:%d %s\n", file, lineno, msg);
+    ERR_print_errors_fp(stderr);
+    /* exit(-1); */ 
+}
+
+/* This array will store all of the mutexes available to OpenSSL. */ 
+static MUTEX_TYPE *mutex_buf= NULL;
+
+static void locking_function(int mode, int n, const char * file, int line)
+{
+    if(mode & CRYPTO_LOCK)
+        MUTEX_LOCK(mutex_buf[n]);
+    else
+        MUTEX_UNLOCK(mutex_buf[n]);
+}
+
+static unsigned long id_function(void)
+{
+    return ((unsigned long)THREAD_ID);
+}
+
+int thread_setup(void)
+{
+    int i;
+
+    mutex_buf = malloc(CRYPTO_num_locks() * sizeof(MUTEX_TYPE));
+    if(!mutex_buf)
+        return 0;
+    for(i = 0;  i < CRYPTO_num_locks();  i++)
+        MUTEX_SETUP(mutex_buf[i]);
+    CRYPTO_set_id_callback(id_function);
+    CRYPTO_set_locking_callback(locking_function);
+    return 1;
+}
+
+int thread_cleanup(void)
+{
+    int i;
+
+    if(!mutex_buf)
+        return 0;
+    CRYPTO_set_id_callback(NULL);
+    CRYPTO_set_locking_callback(NULL);
+    for(i = 0;  i < CRYPTO_num_locks();  i++)
+        MUTEX_CLEANUP(mutex_buf[i]);
+    free(mutex_buf);
+    mutex_buf = NULL;
+    return 1;
+}
+#endif
+
 enum { Workers = 16 };
 TaskDispatch td( Workers );
 
@@ -200,6 +265,11 @@ int main( int argc, char** argv )
         fprintf( stderr, "Destination directory already exists.\n" );
         exit( 1 );
     }
+
+#ifndef _MSC_VER
+    thread_setup();
+#endif
+
     if( curl_global_init( CURL_GLOBAL_ALL ) != 0 )
     {
         fprintf( stderr, "Cannot initialize libcurl.\n" );
@@ -217,6 +287,10 @@ int main( int argc, char** argv )
     } );
     td.Sync();
     printf( "\n" );
+
+#ifndef _MSC_VER
+    thread_cleanup();
+#endif
 
     return 0;
 }
