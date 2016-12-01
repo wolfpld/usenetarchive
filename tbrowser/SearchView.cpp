c147bf0fcd7dd0f2b35946ca43c108548037065d
Bartosz Taudul <wolf.pld@gmail.com>
2016-12-01 20:59:25 +0100

    Basic resize, drawing.

--- tbrowser/SearchView.cpp
+++ tbrowser/SearchView.cpp
@@ -8,8 +8,13 @@ SearchView::SearchView()
 
 void SearchView::Resize()
 {
+    if( !m_active ) return;
+    ResizeView( 0, 1, 0, -2 );
+    Draw();
 }
 
 void SearchView::Draw()
 {
+    wclear( m_win );
+    wnoutrefresh( m_win );
 }
