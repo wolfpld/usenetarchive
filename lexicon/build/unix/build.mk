553be41b41bac51dc940211019302ea058c26e8a
Bartosz Taudul <wolf.pld@gmail.com>
2016-12-03 18:48:25 +0100

    ICU 58 no longer has libicule.

--- lexicon/build/unix/build.mk
+++ lexicon/build/unix/build.mk
@@ -2,7 +2,7 @@ CFLAGS +=
 CXXFLAGS := $(CFLAGS) -std=c++14
 DEFINES +=
 INCLUDES :=
-LIBS := -licule -licuuc
+LIBS := -licuuc
 IMAGE := lexicon
 
 SRC := $(shell egrep 'ClCompile.*cpp"' ../win32/$(IMAGE).vcxproj | sed -e 's/.*\"\(.*\)\".*/\1/' | sed -e 's@\\@/@g')
