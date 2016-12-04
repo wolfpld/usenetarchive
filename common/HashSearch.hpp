bb15ab2c5032c7490e18ef151b76255c87763ebf
Bartosz Taudul <wolf.pld@gmail.com>
2016-12-04 16:52:58 +0100

    Use proper hash size in HashSearch.

--- common/HashSearch.hpp
+++ common/HashSearch.hpp
@@ -15,18 +15,20 @@ public:
     HashSearch( const std::string& data, const std::string& hash, const std::string& hashdata )
         : m_data( data )
         , m_hash( hash, hashdata )
+        , m_mask( m_hash.Size() - 1 )
     {
     }
 
     HashSearch( const FileMapPtrs& data, const FileMapPtrs& hash, const FileMapPtrs& hashdata )
         : m_data( data )
         , m_hash( hash, hashdata )
+        , m_mask( m_hash.Size() - 1 )
     {
     }
 
     int Search( const char* str ) const
     {
-        const auto hash = XXH32( str, strlen( str ), 0 ) & MsgIdHashMask;
+        const auto hash = XXH32( str, strlen( str ), 0 ) & m_mask;
         auto ptr = m_hash[hash];
         const auto num = *ptr++;
         for( uint32_t i=0; i<num; i++ )
@@ -43,6 +45,7 @@ public:
 private:
     FileMap<char> m_data;
     MetaView<uint32_t, uint32_t> m_hash;
+    uint32_t m_mask;
 };
 
 #endif
