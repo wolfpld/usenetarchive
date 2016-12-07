8484b28e86b62331295a704c14affb6fbf9e4112
Bartosz Taudul <wolf.pld@gmail.com>
2016-12-08 00:14:49 +0100

    Copy strings.

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -93,6 +93,8 @@ int main( int argc, char** argv )
     if( Exists( base + "name" ) ) CopyFile( base + "name", dbase + "name" );
     if( Exists( base + "desc_long" ) ) CopyFile( base + "desc_long", dbase + "desc_long" );
     if( Exists( base + "desc_short" ) ) CopyFile( base + "desc_short", dbase + "desc_short" );
+    if( Exists( base + "strings" ) ) CopyFile( base + "strings", dbase + "strings" );
+    if( Exists( base + "strmeta" ) ) CopyFile( base + "strmeta", dbase + "strmeta" );
 
     if( Exists( base + "meta" ) && Exists( base + "data" ) )
     {
