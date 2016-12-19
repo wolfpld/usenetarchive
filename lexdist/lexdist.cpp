02d71b3db9c06faa9c5f0ca70d077ef527bc1d52
Bartosz Taudul <wolf.pld@gmail.com>
2016-12-19 13:21:48 +0100

    Set bits counting in u64.

--- lexdist/lexdist.cpp
+++ lexdist/lexdist.cpp
@@ -16,6 +16,14 @@
 #include "../common/System.hpp"
 #include "../common/TaskDispatch.hpp"
 
+static int CountBits( uint64_t i )
+{
+    i = i - ( (i >> 1) & 0x5555555555555555 );
+    i = ( i & 0x3333333333333333 ) + ( (i >> 2) & 0x3333333333333333 );
+    i = ( (i + (i >> 4) ) & 0x0F0F0F0F0F0F0F0F );
+    return ( i * (0x0101010101010101) ) >> 56;
+}
+
 static int codepointlen( char c )
 {
     if( ( c & 0x80 ) == 0 ) return 1;
