73c5cb4f359586af36775570f3cc5950a2198fb8
Bartosz Taudul <wolf.pld@gmail.com>
2017-06-16 02:02:05 +0200

    Be nice and copy descriptions.

--- update-zstd/update-zstd.cpp
+++ update-zstd/update-zstd.cpp
@@ -175,6 +175,10 @@ int main( int argc, char** argv )
     CreateDirStruct( target );
     CopyFile( szdictfn, zdictfn );
 
+    if( Exists( source + "name" ) ) CopyFile( source + "name", target + "name" );
+    if( Exists( source + "desc_long" ) ) CopyFile( source + "desc_long", target + "desc_long" );
+    if( Exists( source + "desc_short" ) ) CopyFile( source + "desc_short", target + "desc_short" );
+
     FILE* zmeta = fopen( zmetafn.c_str(), "wb" );
     FILE* zdata = fopen( zdatafn.c_str(), "wb" );
 
