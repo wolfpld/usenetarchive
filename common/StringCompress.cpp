6e675a1fb65a9f81ea2fcda716144fd3d74939a7
Bartosz Taudul <wolf.pld@gmail.com>
2017-07-23 17:06:34 +0200

    Only scan n-gram tables, if match can be found.

--- common/StringCompress.cpp
+++ common/StringCompress.cpp
@@ -682,19 +682,25 @@ size_t StringCompress::Pack( const char* in, uint8_t* out ) const
     {
         if( *in != '@' )
         {
-            auto it3 = std::lower_bound( TrigramCompressionModel3, TrigramCompressionModel3 + TrigramSize3, in, [] ( const auto& l, const auto& r ) { return strncmp( l, r, 3 ) < 0; } );
-            if( it3 != TrigramCompressionModel3 + TrigramSize3 && strncmp( *it3, in, 3 ) == 0 )
+            if( *in == '$' ||
+                *in == '.' ||
+                ( *in >= '0' && *in <= '9' ) ||
+                ( *in >= 'a' && *in <= 'v' ) )
             {
-                *out++ = TrigramCompressionIndex3[it3 - TrigramCompressionModel3];
-                in += 3;
-                continue;
-            }
-            auto it2 = std::lower_bound( TrigramCompressionModel2, TrigramCompressionModel2 + TrigramSize2, in, [] ( const auto& l, const auto& r ) { return strncmp( l, r, 2 ) < 0; } );
-            if( it2 != TrigramCompressionModel2 + TrigramSize2 && strncmp( *it2, in, 2 ) == 0 )
-            {
-                *out++ = TrigramCompressionIndex2[it2 - TrigramCompressionModel2];
-                in += 2;
-                continue;
+                auto it3 = std::lower_bound( TrigramCompressionModel3, TrigramCompressionModel3 + TrigramSize3, in, [] ( const auto& l, const auto& r ) { return strncmp( l, r, 3 ) < 0; } );
+                if( it3 != TrigramCompressionModel3 + TrigramSize3 && strncmp( *it3, in, 3 ) == 0 )
+                {
+                    *out++ = TrigramCompressionIndex3[it3 - TrigramCompressionModel3];
+                    in += 3;
+                    continue;
+                }
+                auto it2 = std::lower_bound( TrigramCompressionModel2, TrigramCompressionModel2 + TrigramSize2, in, [] ( const auto& l, const auto& r ) { return strncmp( l, r, 2 ) < 0; } );
+                if( it2 != TrigramCompressionModel2 + TrigramSize2 && strncmp( *it2, in, 2 ) == 0 )
+                {
+                    *out++ = TrigramCompressionIndex2[it2 - TrigramCompressionModel2];
+                    in += 2;
+                    continue;
+                }
             }
             assert( *in >= 32 && *in <= 126 );
             *out++ = *in++;
