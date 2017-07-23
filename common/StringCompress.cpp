e9f6a6e5e762f86ae20880efbf7c04a73efe4411
Bartosz Taudul <wolf.pld@gmail.com>
2017-07-23 02:16:02 +0200

    Add host compression.

--- common/StringCompress.cpp
+++ common/StringCompress.cpp
@@ -1,14 +1,85 @@
 #include <algorithm>
 #include <assert.h>
+#include <string.h>
+#include <unordered_map>
 
 #include "StringCompress.hpp"
 
-size_t StringCompress::Pack( const char* in, uint8_t* out )
+StringCompress::StringCompress( const std::set<const char*, CharUtil::LessComparator>& strings )
+{
+    std::unordered_map<const char*, uint64_t, CharUtil::Hasher, CharUtil::Comparator> hosts;
+
+    for( auto v : strings )
+    {
+        while( *v != '@' && *v != '\0' ) v++;
+        if( *v == '@' )
+        {
+            auto it = hosts.find( v );
+            if( it == hosts.end() )
+            {
+                hosts.emplace( v, 1 );
+            }
+            else
+            {
+                it->second++;
+            }
+        }
+    }
+
+    std::vector<std::unordered_map<const char*, uint64_t, CharUtil::Hasher, CharUtil::Comparator>::iterator> hvec;
+    hvec.reserve( hosts.size() );
+    for( auto it = hosts.begin(); it != hosts.end(); ++it )
+    {
+        hvec.emplace_back( it );
+    }
+    std::sort( hvec.begin(), hvec.end(), [] ( const auto& l, const auto& r ) { return l->second * ( strlen( l->first ) - 1 ) > r->second * ( strlen( r->first ) - 1 ); } );
+
+    m_dataLen = 0;
+    m_maxHost = std::min<int>( hvec.size(), 255 );
+    for( int i=0; i<m_maxHost; i++ )
+    {
+        m_dataLen += strlen( hvec[i]->first ) + 1;
+    }
+
+    auto data = new char[m_dataLen];
+    auto ptr = data;
+    for( int i=0; i<m_maxHost; i++ )
+    {
+        m_hostOffset[i] = ptr - data;
+        auto len = strlen( hvec[i]->first );
+        memcpy( ptr, hvec[i]->first, len+1 );
+        ptr += len+1;
+    }
+    m_data = data;
+
+    for( int i=0; i<m_maxHost; i++ )
+    {
+        m_hostLookup[i] = i;
+    }
+    std::sort( m_hostLookup, m_hostLookup+m_maxHost, [&hvec] ( const auto& l, const auto& r ) { return strcmp( hvec[l]->first, hvec[r]->first ) < 0; } );
+}
+
+StringCompress::~StringCompress()
+{
+    delete[] m_data;
+}
+
+size_t StringCompress::Pack( const char* in, uint8_t* out ) const
 {
     const uint8_t* refout = out;
 
     while( *in != 0 )
     {
+        if( *in == '@' )
+        {
+            auto it = std::lower_bound( m_hostLookup, m_hostLookup + m_maxHost, in, [this] ( const auto& l, const auto& r ) { return strcmp( m_data + m_hostOffset[l], r ) < 0; } );
+            if( it != m_hostLookup + m_maxHost && strncmp( m_data + m_hostOffset[*it], in, 3 ) == 0 )
+            {
+                *out++ = 1;
+                *out++ = *it;
+                break;
+            }
+        }
         auto it3 = std::lower_bound( TrigramCompressionModel3, TrigramCompressionModel3 + TrigramSize3, in, [] ( const auto& l, const auto& r ) { return strncmp( l, r, 3 ) < 0; } );
         if( it3 != TrigramCompressionModel3 + TrigramSize3 && strncmp( *it3, in, 3 ) == 0 )
         {
@@ -32,12 +103,20 @@ size_t StringCompress::Pack( const char* in, uint8_t* out )
     return out - refout;
 }
 
-size_t StringCompress::Unpack( const uint8_t* in, char* out )
+size_t StringCompress::Unpack( const uint8_t* in, char* out ) const
 {
     const char* refout = out;
 
     while( *in != 0 )
     {
+        if( *in == 1 )
+        {
+            in++;
+            const char* dec = m_data + m_hostOffset[*in];
+            while( *dec != '\0' ) *out++ = *dec++;
+            assert( *++in == 0 );
+            break;
+        }
         const char* dec = TrigramDecompressionModel[*in++];
         while( *dec != '\0' ) *out++ = *dec++;
     }
--- common/StringCompress.hpp
+++ common/StringCompress.hpp
@@ -1,15 +1,35 @@
 #ifndef __STRING_COMPRESSION_MODEL__
 #define __STRING_COMPRESSION_MODEL__
 
+#include <set>
 #include <stdint.h>
+#include <string>
+#include <vector>
+
+#include "CharUtil.hpp"
 
 class StringCompress
 {
 public:
-    size_t Pack( const char* in, uint8_t* out );
-    size_t Unpack( const uint8_t* in, char* out );
-};
+    StringCompress( const std::set<const char*, CharUtil::LessComparator>& strings );
+    ~StringCompress();
 
+    size_t Pack( const char* in, uint8_t* out ) const;
+    size_t Unpack( const uint8_t* in, char* out ) const;
+
+private:
+    StringCompress( const StringCompress& ) = delete;
+    StringCompress( StringCompress&& ) = delete;
+
+    StringCompress& operator=( const StringCompress& ) = delete;
+    StringCompress& operator=( StringCompress&& ) = delete;
+
+    const char* m_data;
+    uint32_t m_dataLen;
+    uint8_t m_maxHost;
+    uint8_t m_hostLookup[255];
+    uint32_t m_hostOffset[255];
+};
 
 static const char* TrigramDecompressionModel[256] = {
 nullptr,   // 0: end of string
