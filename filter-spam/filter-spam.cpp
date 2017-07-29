d303fe9f28946f13d88b4589998ae2957f178048
Bartosz Taudul <wolf.pld@gmail.com>
2017-07-29 16:23:32 +0200

    Display more message context.

--- filter-spam/filter-spam.cpp
+++ filter-spam/filter-spam.cpp
@@ -428,7 +428,7 @@ int main( int argc, char** argv )
             }
             while( !done );
 
-            for( int i=0; i<20; i++ )
+            for( int i=0; i<30; i++ )
             {
                 end = ptr;
                 while( *end != '\n' && *end != '\0' ) end++;
