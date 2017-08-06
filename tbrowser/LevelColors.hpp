8ef6333dc9e4806dec42eea3a628c53cf2818185
Bartosz Taudul <wolf.pld@gmail.com>
2017-08-06 20:35:03 +0200

    Document QuoteFlags.

--- tbrowser/LevelColors.hpp
+++ tbrowser/LevelColors.hpp
@@ -3,12 +3,13 @@
 
 #include <curses.h>
 
+// colors for quotation levels
 static const chtype QuoteFlags[] = {
-    COLOR_PAIR( 5 ),
-    COLOR_PAIR( 3 ),
-    COLOR_PAIR( 6 ) | A_BOLD,
-    COLOR_PAIR( 2 ),
-    COLOR_PAIR( 4 ),
+    COLOR_PAIR( 5 ),            // T_Quote1
+    COLOR_PAIR( 3 ),            // T_Quote2
+    COLOR_PAIR( 6 ) | A_BOLD,   // T_Quote3
+    COLOR_PAIR( 2 ),            // T_Quote4
+    COLOR_PAIR( 4 ),            // T_Quote5
 };
 
 enum { NumQuoteFlags = sizeof( QuoteFlags ) / sizeof( chtype ) };
