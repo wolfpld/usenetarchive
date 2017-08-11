e4396439f2749d3b12555c1b9edbdd9b96460b7e
Bartosz Taudul <wolf.pld@gmail.com>
2017-08-11 02:02:42 +0200

    Link repack-lz4 with libpthread.

--- repack-lz4/build/unix/build.mk
+++ repack-lz4/build/unix/build.mk
@@ -2,7 +2,7 @@ CFLAGS +=
 CXXFLAGS := $(CFLAGS) -std=c++14
 DEFINES +=
 INCLUDES :=
-LIBS :=
+LIBS := -lpthread
 IMAGE := repack-lz4
 
 SRC := $(shell egrep 'ClCompile.*cpp"' ../win32/$(IMAGE).vcxproj | sed -e 's/.*\"\(.*\)\".*/\1/' | sed -e 's@\\@/@g')
