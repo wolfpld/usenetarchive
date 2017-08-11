dcac81107d82e44ed098faba33e2233cf62a64aa
Bartosz Taudul <wolf.pld@gmail.com>
2017-08-11 02:07:39 +0200

    Link threadify with libpthread.

--- threadify/build/unix/build.mk
+++ threadify/build/unix/build.mk
@@ -2,7 +2,7 @@ CFLAGS +=
 CXXFLAGS := $(CFLAGS) -std=c++14
 DEFINES +=
 INCLUDES := -I../../../contrib/zstd/common -I../../../contrib/zstd
-LIBS := -licuuc
+LIBS := -licuuc -lpthread
 IMAGE := threadify
 
 SRC := $(shell egrep 'ClCompile.*cpp"' ../win32/$(IMAGE).vcxproj | sed -e 's/.*\"\(.*\)\".*/\1/' | sed -e 's@\\@/@g')
