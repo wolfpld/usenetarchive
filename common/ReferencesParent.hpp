a8b74d8958729d023cd83a9ba8e3b557f9f3bb8a
Bartosz Taudul <wolf.pld@gmail.com>
2017-08-17 21:55:58 +0200

    All references getter.

--- common/ReferencesParent.hpp
+++ common/ReferencesParent.hpp
@@ -2,6 +2,8 @@
 #define __REFERENCESPARENT_HPP__
 
 #include <assert.h>
+#include <string>
+#include <vector>
 
 #include "MessageLogic.hpp"
 #include "StringCompress.hpp"
@@ -80,6 +82,52 @@ inline int GetParentFromReferences( const char* post, const StringCompress& comp
         {
             return idx;
         }
+
+        buf -= 2;
+    }
+}
+
+std::vector<std::string> GetAllReferences( const char* post, const StringCompress& compress )
+{
+    std::vector<std::string> ret;
+
+    auto buf = FindReferences( post );
+    if( *buf == '\n' ) return ret;
+
+    const auto terminate = buf;
+    while( *buf != '\n' ) buf++;
+    if( buf == terminate ) return ret;
+    buf--;
+    // Handle "In-Reply-To: <msg@id> from a@x.y at 12/12/1999"
+    while( buf > terminate && ( *buf != '>' && *buf != '<' ) ) buf--;
+    if( buf <= terminate || *buf == '<' ) return ret;
+    for(;;)
+    {
+        while( buf > terminate && ( *buf == ' ' || *buf == '\t' ) ) buf--;
+        if( buf <= terminate || *buf != '>' )
+        {
+            return ret;
+        }
+        auto end = buf;
+        buf--;
+        while( buf > terminate && *buf != '<' ) buf--;
+        if( *buf != '<' )
+        {
+            return ret;
+        }
+        buf++;
+        assert( end - buf < 1024 );
+        if( !IsMsgId( buf, end ) )
+        {
+            return ret;
+        }
+
+        char tmp[1024];
+        ValidateMsgId( buf, end, tmp );
+
+        uint8_t pack[1024];
+        compress.Pack( tmp, pack );
+        ret.emplace_back( (char*)pack );
 
         buf -= 2;
     }
