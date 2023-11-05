wolfpld/usenetarchive

commit ea1114c84937e1ff5e6be83f3ff3137cd2a30ae1

Integrate lexhash with lexicon.
This is done in order to facilitate linear search over lexicon strings.

lexicon/lexicon.cpp
@@ -9,12 +9,14 @@
 #include <unordered_map>
 #include <vector>
 
+#include "../contrib/xxhash/xxhash.h"
 #include "../common/Alloc.hpp"
 #include "../common/ICU.hpp"
 #include "../common/LexiconTypes.hpp"
 #include "../common/MetaView.hpp"
 #include "../common/MessageLogic.hpp"
 #include "../common/MessageView.hpp"
+#include "../common/MsgIdHash.hpp"
 #include "../common/String.hpp"
 
 #define SPP_INCLUDE_SPP_ALLOC 1
@@ -213,14 +215,105 @@ int main( int argc, char** argv )
     printf( "\nSaving...\n" );
     fflush( stdout );
 
-    FILE* fmeta = fopen( ( base + "lexmeta" ).c_str(), "wb" );
+    auto wordNum = data.size();
+    auto hashbits = MsgIdHashBits( wordNum, 90 );
+    auto hashsize = MsgIdHashSize( hashbits );
+    auto hashmask = MsgIdHashMask( hashbits );
+
+    auto hashdata = new uint32_t[hashsize];
+    auto distance = new uint8_t[hashsize];
+    memset( distance, 0xFF, hashsize );
+    uint8_t distmax = 0;
+
+    int cnt = 0;
+    std::vector<const char*> strings;
+    strings.reserve( wordNum );
+    for( auto& v : data )
+    {
+        if( ( cnt & 0xFFF ) == 0 )
+        {
+            printf( "%i/%i\r", cnt, wordNum );
+            fflush( stdout );
+        }
+
+        const auto& s = v.first;
+        strings.emplace_back( s.c_str() );
+
+        uint32_t hash = XXH32( s.c_str(), s.size(), 0 ) & hashmask;
+        uint8_t dist = 0;
+        uint32_t idx = cnt;
+        for(;;)
+        {
+            if( distance[hash] == 0xFF )
+            {
+                if( distmax < dist ) distmax = dist;
+                distance[hash] = dist;
+                hashdata[hash] = idx;
+                break;
+            }
+            if( distance[hash] < dist )
+            {
+                if( distmax < dist ) distmax = dist;
+                std::swap( distance[hash], dist );
+                std::swap( hashdata[hash], idx );
+            }
+            dist++;
+            assert( dist < std::numeric_limits<uint8_t>::max() );
+            hash = (hash+1) & hashmask;
+        }
+
+        cnt++;
+    }
+
+    printf( "\n" );
+
+    FILE* fhashdata = fopen( ( base + "lexhashdata" ).c_str(), "wb" );
+    fwrite( &distmax, 1, 1, fhashdata );
+    fclose( fhashdata );
+
+    FILE* fhash = fopen( ( base + "lexhash" ).c_str(), "wb" );
     FILE* fstr = fopen( ( base + "lexstr" ).c_str(), "wb" );
+
+    uint32_t zero = 0;
+    uint32_t stroffset = fwrite( &zero, 1, 1, fstr );
+
+    auto offsetData = new uint32_t[wordNum];
+
+    cnt = 0;
+    for( int i=0; i<hashsize; i++ )
+    {
+        if( ( i & 0x3FFF ) == 0 )
+        {
+            printf( "%i/%i\r", i, hashsize );
+            fflush( stdout );
+        }
+
+        if( distance[i] == 0xFF )
+        {
+            fwrite( &zero, 1, sizeof( uint32_t ), fhash );
+            fwrite( &zero, 1, sizeof( uint32_t ), fhash );
+        }
+        else
+        {
+            fwrite( &stroffset, 1, sizeof( uint32_t ), fhash );
+            fwrite( hashdata+i, 1, sizeof( uint32_t ), fhash );
+
+            offsetData[hashdata[i]] = stroffset;
+            cnt++;
+            auto str = strings[hashdata[i]];
+            stroffset += fwrite( str, 1, strlen( str ) + 1, fstr );
+        }
+    }
+    assert( cnt == hashsize );
+    fclose( fhash );
+    fclose( fstr );
+
+    printf( "\n" );
+
+    FILE* fmeta = fopen( ( base + "lexmeta" ).c_str(), "wb" );
     FILE* fdata = fopen( ( base + "lexdata" ).c_str(), "wb" );
     FILE* fhit = fopen( ( base + "lexhit" ).c_str(), "wb" );
 
-    uint8_t zero = 0;
-
-    uint32_t ostr = fwrite( &zero, 1, 1, fstr );
     uint32_t odata = 0;
     uint32_t ohit = 0;
 
@@ -233,16 +326,11 @@ int main( int argc, char** argv )
             printf( "%i/%i\r", idx, dataSize );
             fflush( stdout );
         }
-        idx++;
 
         uint32_t dsize = v.second.size();
-        fwrite( &ostr, 1, sizeof( uint32_t ), fmeta );
+        fwrite( &offsetData[idx], 1, sizeof( uint32_t ), fmeta );
         fwrite( &odata, 1, sizeof( uint32_t ), fmeta );
         fwrite( &dsize, 1, sizeof( uint32_t ), fmeta );
-
-        auto strsize = v.first.size() + 1;
-        fwrite( v.first.c_str(), 1, strsize, fstr );
-        ostr += strsize;
 
         for( auto& d : v.second )
         {
@@ -270,12 +358,13 @@ int main( int argc, char** argv )
             }
         }
         odata += sizeof( uint32_t ) * dsize * 2;
+
+        idx++;
     }
 
     printf( "\n" );
 
     fclose( fmeta );
-    fclose( fstr );
     fclose( fdata );
     fclose( fhit );
 

Makefile
@@ -12,7 +12,6 @@ TOOLS := \
 	import-source-mbox \
 	kill-duplicates \
 	lexdist \
-	lexhash \
 	lexicon \
 	lexsort \
 	lexstats \

build/usenet.sln
@@ -33,8 +33,6 @@ Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "lexstats", "..\lexstats\bui
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "lexdist", "..\lexdist\build\win32\lexdist.vcxproj", "{5396A9AB-DDF0-4D66-B026-78E313F765D9}"
 EndProject
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "lexhash", "..\lexhash\build\win32\lexhash.vcxproj", "{CFFC6922-D083-492F-A44A-CE618DB0D144}"
-EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "lexsort", "..\lexsort\build\win32\lexsort.vcxproj", "{1C7F92F9-4D03-4025-BB5D-9BB47CF90A5F}"
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "filter-newsgroups", "..\filter-newsgroups\build\win32\filter-newsgroups.vcxproj", "{96CE0604-1E5E-4877-A02E-B4C12216201C}"
@@ -131,10 +129,6 @@ Global
 		{5396A9AB-DDF0-4D66-B026-78E313F765D9}.Debug|x64.Build.0 = Debug|x64
 		{5396A9AB-DDF0-4D66-B026-78E313F765D9}.Release|x64.ActiveCfg = Release|x64
 		{5396A9AB-DDF0-4D66-B026-78E313F765D9}.Release|x64.Build.0 = Release|x64
-		{CFFC6922-D083-492F-A44A-CE618DB0D144}.Debug|x64.ActiveCfg = Debug|x64
-		{CFFC6922-D083-492F-A44A-CE618DB0D144}.Debug|x64.Build.0 = Debug|x64
-		{CFFC6922-D083-492F-A44A-CE618DB0D144}.Release|x64.ActiveCfg = Release|x64
-		{CFFC6922-D083-492F-A44A-CE618DB0D144}.Release|x64.Build.0 = Release|x64
 		{1C7F92F9-4D03-4025-BB5D-9BB47CF90A5F}.Debug|x64.ActiveCfg = Debug|x64
 		{1C7F92F9-4D03-4025-BB5D-9BB47CF90A5F}.Debug|x64.Build.0 = Debug|x64
 		{1C7F92F9-4D03-4025-BB5D-9BB47CF90A5F}.Release|x64.ActiveCfg = Release|x64

doc/pipeline.dot
@@ -43,8 +43,7 @@ digraph
     msgmeta -> connectivity;
     connectivity -> repack;
     repack -> lexicon;
-    lexicon -> lexhash;
-    lexhash -> lexsort;
+    lexicon -> lexsort;
     lexsort -> sort;
     sort -> lexdist;
     sort -> verify;

lexhash/build/unix/Makefile
@@ -1,12 +1,0 @@
-all: debug
-
-debug:
-	@+make -f debug.mk all
-
-release:
-	@+make -f release.mk all
-
-clean:
-	@+make -f build.mk clean
-
-.PHONY: all clean debug release

lexhash/build/unix/build.mk
@@ -1,45 +1,0 @@
-CFLAGS += 
-CXXFLAGS := $(CFLAGS) -std=c++14
-DEFINES +=
-INCLUDES :=
-LIBS :=
-IMAGE := lexhash
-
-SRC := $(shell egrep 'ClCompile.*cpp"' ../win32/$(IMAGE).vcxproj | sed -e 's/.*\"\(.*\)\".*/\1/' | sed -e 's@\\@/@g')
-SRC2 := $(shell egrep 'ClCompile.*c"' ../win32/$(IMAGE).vcxproj | sed -e 's/.*\"\(.*\)\".*/\1/' | sed -e 's@\\@/@g')
-OBJ := $(SRC:%.cpp=%.o)
-OBJ2 := $(SRC2:%.c=%.o)
-
-all: $(IMAGE)
-
-%.o: %.cpp
-	$(CXX) -c $(INCLUDES) $(CXXFLAGS) $(DEFINES) $< -o $@
-
-%.d : %.cpp
-	@echo Resolving dependencies of $<
-	@mkdir -p $(@D)
-	@$(CXX) -MM $(INCLUDES) $(CXXFLAGS) $(DEFINES) $< > $@.$$$$; \
-	sed 's,.*\.o[ :]*,$(<:.cpp=.o) $@ : ,g' < $@.$$$$ > $@; \
-	rm -f $@.$$$$
-
-%.o: %.c
-	$(CC) -c $(INCLUDES) $(CFLAGS) $(DEFINES) $< -o $@
-
-%.d : %.c
-	@echo Resolving dependencies of $<
-	@mkdir -p $(@D)
-	@$(CC) -MM $(INCLUDES) $(CFLAGS) $(DEFINES) $< > $@.$$$$; \
-	sed 's,.*\.o[ :]*,$(<:.c=.o) $@ : ,g' < $@.$$$$ > $@; \
-	rm -f $@.$$$$
-
-$(IMAGE): $(OBJ) $(OBJ2)
-	$(CXX) $(CXXFLAGS) $(DEFINES) $(OBJ) $(OBJ2) $(LIBS) -o $@
-
-ifneq "$(MAKECMDGOALS)" "clean"
--include $(SRC:.cpp=.d) $(SRC2:.c=.d)
-endif
-
-clean:
-	rm -f $(OBJ) $(OBJ2) $(SRC:.cpp=.d) $(SRC2:.c=.d) $(IMAGE)
-
-.PHONY: clean all

lexhash/build/unix/debug.mk
@@ -1,10 +1,0 @@
-ARCH := $(shell uname -m)
-
-CFLAGS := -g3 -Wall
-DEFINES := -DDEBUG
-
-ifeq ($(ARCH),x86_64)
-CFLAGS += -msse4.1
-endif
-
-include build.mk

commit 85dfd35ca3cffd12c081cc55d84c3ba7e35b8b1f

Fix assert regression introduced in ea1114c8.

lexicon/lexicon.cpp
@@ -327,7 +327,7 @@ int main( int argc, char** argv )
             stroffset += fwrite( str, 1, strlen( str ) + 1, fstr );
         }
     }
-    assert( cnt == hashsize );
+    assert( cnt == data.size() );
     fclose( fhash );
     fclose( fstr );
 